cade42e6444e149bebcec22436732df4ad2891d7
MOVZX <movzx@yahoo.com>
2017-08-25 20:07:14 +0300

    Update Dalvik/ART Heap
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- init/init_msm.cpp
+++ init/init_msm.cpp
@@ -42,15 +42,15 @@ void check_device()
 
     if (sys.totalram > 2048ull * 1024 * 1024) {
         // from - phone-xxhdpi-3072-dalvik-heap.mk
-        heapstartsize = "8m";
-        heapgrowthlimit = "288m";
-        heapsize = "768m";
-        heapminfree = "512k";
+        heapstartsize = "16m";
+        heapgrowthlimit = "256m";
+        heapsize = "512m";
+        heapminfree = "4m";
     } else {
         // from - phone-xxhdpi-2048-dalvik-heap.mk
-        heapstartsize = "16m";
+        heapstartsize = "8m";
         heapgrowthlimit = "192m";
-        heapsize = "512m";
+        heapsize = "448m";
         heapminfree = "2m";
     }
 }
