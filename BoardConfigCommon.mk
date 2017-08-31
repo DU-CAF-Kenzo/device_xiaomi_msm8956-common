59b3403070a7a3b6e13fdde4e9b2368ca9ce6ce7
Wight554 <wight554@gmail.com>
2017-08-31 14:25:09 +0000

    Switch to UberTC for kernel

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -54,6 +54,7 @@ TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8956
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-6.x/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # ANT+
